17da2693db430069317ea253ba790406d0451096
Lucas Yago <lucasyago63@gmail.com>
2022-07-28 12:06:11 -0300

    Adiciona layout base do formulario de convidado

--- app/src/main/java/com/lucasyago/guest/GuestFormActivity.kt
+++ app/src/main/java/com/lucasyago/guest/GuestFormActivity.kt
@@ -2,10 +2,26 @@ package com.lucasyago.guest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.lucasyago.guest.databinding.ActivityGuestFormBinding
 
-class GuestFormActivity : AppCompatActivity() {
+class GuestFormActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityGuestFormBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_guest_form)
+
+        binding = ActivityGuestFormBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonSave.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.button_save){
+            Toast.makeText(this,"clicou em salvar!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
