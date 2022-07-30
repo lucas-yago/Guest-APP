aa5ac31253de432f165dd6107f5c1155bdc702d3
Lucas Yago <lucasyago63@gmail.com>
2022-07-30 18:38:34 -0300

    cria banco de dados

--- app/src/main/java/com/lucasyago/guest/repository/GuestDataBase.kt
+++ app/src/main/java/com/lucasyago/guest/repository/GuestDataBase.kt
@@ -1,4 +1,26 @@
 package com.lucasyago.guest.repository
 
-class GuestDataBase {
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class GuestDataBase(context: Context) : SQLiteOpenHelper(context, NAME, null, VERSION) {
+
+    companion object {
+        private const val NAME = "guestdb"
+        private const val VERSION = 1
+    }
+
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL("CREATE TABLE guest (" +
+                "id integer primary key autoincrement," +
+                "name text," +
+                "presence integer);")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        TODO("Not yet implemented")
+    }
+
+
 }
--- app/src/main/java/com/lucasyago/guest/viewmodel/GuestFormViewModel.kt
+++ app/src/main/java/com/lucasyago/guest/viewmodel/GuestFormViewModel.kt
@@ -1,11 +1,15 @@
 package com.lucasyago.guest.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
 import com.lucasyago.guest.repository.GuestRepository
 
-class GuestFormViewModel: ViewModel() {
+class GuestFormViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val repository = GuestRepository.getInstance()
+    private val repository = GuestRepository.getInstance(application)
+
+
 
 
 }
